a4100ee740407e503a9afc49848d7d755c6f726e
hmugera <mugerahughes@gmail.com>
2020-12-30 23:07:16 +0300

    setting up framer motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,24 +1,48 @@
 import React from "react";
 import home1 from "../img/home1.png";
 
+import { motion } from "framer-motion";
 import { Wrapper, Description, Image, Hide } from "../styles";
 function AboutSection() {
+	//Framer motion variance
+	const titleAnim = {
+		hidden: { opacity: 0 },
+		show: { opacity: 1, transition: { duration: 2 } },
+	};
+
+	const container = {
+		hidden: { x: 100 },
+		show: {
+			x: 0,
+			transition: {
+				duration: 0.75,
+				ease: "easeOut",
+				staggerChildren: 1,
+				// when: "beforeChildren",
+			},
+		},
+	};
 	return (
 		<Wrapper>
 			<Description>
-				<div className="title">
+				<motion.div
+					className="title"
+					variants={container}
+					animate="show"
+					initial="hidden"
+				>
 					<Hide>
-						<h2>Let us help you capture </h2>
+						<motion.h2 variants={titleAnim}>Let us help you capture </motion.h2>
 					</Hide>
 					<Hide>
-						<h2>
+						<motion.h2 variants={titleAnim}>
 							your best <span>memories </span>
-						</h2>
+						</motion.h2>
 					</Hide>
 					<Hide>
-						<h2>forever.</h2>
+						<motion.h2 variants={titleAnim}>forever.</motion.h2>
 					</Hide>
-				</div>
+				</motion.div>
 				<p>
 					Contact us for any photography or videography ideas.We have
 					professionald with amazing skills
