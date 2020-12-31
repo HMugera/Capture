ddb8f82db14d1baefef58a4f017abac42fa2995b
hmugera <mugerahughes@gmail.com>
2020-12-31 13:39:15 +0300

    added animation in faq section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { Wrapper } from "../styles";
+import Toggle from "./Toggle";
+
+import { AnimateSharedLayout } from "framer-motion";
 
 function FaqSection() {
 	return (
@@ -8,50 +11,44 @@ function FaqSection() {
 			<h2>
 				Any Questions<span>FAQ</span>
 			</h2>
-			<div className="question">
-				<h4>How Do I Start</h4>
-				<div className="answer">
-					<p>Lorem ipsum dolor sit.</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
-						nobis.
-					</p>
-				</div>
-				<div className="faq-line"></div>
-			</div>
-			<div className="question">
-				<h4>Daily schedule</h4>
-				<div className="answer">
-					<p>Lorem ipsum dolor sit.</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
-						nobis.
-					</p>
-				</div>
-				<div className="faq-line"></div>
-			</div>
-			<div className="question">
-				<h4>Diffrent payment methods </h4>
-				<div className="answer">
-					<p>Lorem ipsum dolor sit.</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
-						nobis.
-					</p>
-				</div>
-				<div className="faq-line"></div>
-			</div>
-			<div className="question">
-				<h4>What products do you offer</h4>
-				<div className="answer">
-					<p>Lorem ipsum dolor sit.</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
-						nobis.
-					</p>
-				</div>
-				<div className="faq-line"></div>
-			</div>
+			<AnimateSharedLayout>
+				<Toggle title="How Do I Start">
+					<div className="answer">
+						<p>Lorem ipsum dolor sit.</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+							nobis.
+						</p>
+					</div>
+				</Toggle>
+				<Toggle title="Daily schedule">
+					<div className="answer">
+						<p>Lorem ipsum dolor sit.</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+							nobis.
+						</p>
+					</div>
+				</Toggle>
+				<Toggle title="Diffrent payment methods">
+					<div className="answer">
+						<p>Lorem ipsum dolor sit.</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+							nobis.
+						</p>
+					</div>
+				</Toggle>
+				<Toggle title="What products do you offer">
+					<div className="answer">
+						<p>Lorem ipsum dolor sit.</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+							nobis.
+						</p>
+					</div>
+				</Toggle>
+			</AnimateSharedLayout>
 		</Faq>
 	);
 }
